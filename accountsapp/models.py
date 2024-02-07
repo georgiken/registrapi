d992b83ef035c18b8bd3f051b5da6f1bf057f447
georgiken <georgiy.voronin.05@mail.ru>
2024-02-07 12:30:31 +0300

    added param 'is_verified' to CustomUser model

--- accountsapp/models.py
+++ accountsapp/models.py
@@ -7,4 +7,5 @@ class CustomUser(AbstractUser):
     username = models.CharField(unique=True, max_length=128)
     email = models.EmailField(max_length=128)
     password = models.CharField(max_length=128)
+    is_verified = models.BooleanField(default=False)
 
